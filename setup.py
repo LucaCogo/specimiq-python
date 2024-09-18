3887c6797625bce3782ea7791ae707468d1fcb63
Luca Cogo <36960367+LucaCogo@users.noreply.github.com>
2024-09-18 17:23:38 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+name='specimiq-python',
+version='0.1.0',
+author='Luca Cogo',
+author_email='lucacogo997@gmail.com',
+description='A short description of your package',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'Operating System :: OS Independent',
+]
+)
