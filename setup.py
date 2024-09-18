b3101419efb30bcf8e988f89a76b53257abf894c
LucaCogo <lucacogo997@gmail.com>
2024-09-18 17:28:48 +0200

    Edit setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,6 @@ name='specimiq-python',
 version='0.1.0',
 author='Luca Cogo',
 author_email='lucacogo997@gmail.com',
-description='A short description of your package',
+description='Python library for handling Specim IQ files ',
 packages=find_packages(),
-classifiers=[
-'Programming Language :: Python :: 3',
-'Operating System :: OS Independent',
-]
 )
